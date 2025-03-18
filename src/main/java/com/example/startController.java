f5cc43a3d4986b741c881d135d580c6f8b0ef7e7
Pók László <pokica6@gmail.com>
2025-03-18 11:09:04 +0100

    startScene.fxml hozzáadva

---
+++ src/main/java/com/example/startController.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class startController {
+
+    @FXML
+    private ComboBox<?> betComboBox;
+
+    @FXML
+    private TableView<?> betTable;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    void onClickBetButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClick_raceButton(ActionEvent event) {
+
+    }
+
+}
